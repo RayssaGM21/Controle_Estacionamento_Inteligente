e454586413ee353106fc84eaaca9413ceaf88ee4
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-12 16:04:40 -0300

    feat: add moto e caminhão Rules

---
+++ src/Domain/Rules/MotorcycleRules.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Rules;
+
+use App\Domain\TollRules;
+
+final class MotorcycleRules implements TollRules
+{
+    public function calculate()
+    {
+        return;
+    }
+}
---
+++ src/Domain/Rules/TruckRules.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Rules;
+
+use App\Domain\TollRules;
+
+final class TruckRules implements TollRules
+{
+    public function calculate()
+    {
+        return;
+    }
+}

0eeffe233b21242907ad95876ceef8d03c01ae3e
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-14 18:55:24 -0300

    feat: adicionado Service de Parking(vazio)

---
+++ src/Domain/Application/ParkingSessionService.php
@@ -0,0 +1,42 @@
+<?php 
+
+declare(strict_type=1);
+
+namespace App\Application;
+
+use App\Domain\ParkingSession;
+use App\Domain\ParkingSessionRepository;
+
+final class ParkingSessionService 
+{
+    public function __construct()
+    {
+
+    }
+
+    public function add(array $input)
+    {
+        
+    }
+
+    public function delete(int $id)
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function getAll()
+    {
+
+    }
+
+    public function getById(int $id)
+    {
+
+    }
+}
+

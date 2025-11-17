bee82fdc73a993cc366f7bf45af3072e62032191
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-17 16:40:48 -0300

    refactor: teste de cores

--- public/register.php
+++ public/register.php
@@ -20,41 +20,41 @@ $result = $parkingService->add($_POST);
 </head>
 <body class="bg-[url('./images/parking.jpg')] bg-cover min-h-screen flex justify-center items-center p-4">
     <div class="w-full max-w-4xl">
-        <div class="bg-blue-950/20 backdrop-blur rounded-t-2xl p-10 pb-0 border-b-0 border border-neutral-700/50">
+        <div class="bg-white/10   backdrop-blur rounded-t-2xl p-10 pb-0 border-b-0 border border-blue-500/30">
             <h1 class="text-stone-100 font-bold text-3xl tracking-wide">Registro de Sessão</h1>
             <p class="text-gray-400 mt-1">Sistema de controle de estacionamento</p>
         </div>
-        <div class="bg-blue-950/20 backdrop-blur rounded-b-2xl p-10 pt-6 border-t-0 shadow-sm shadow-blue-100/10 border border-neutral-700/50">
-            <div class="border border-blue-500/20 mb-5"></div>
+        <div class="bg-white/10   backdrop-blur rounded-b-2xl p-10 pt-6 border-t-0 shadow-sm shadow-blue-100/10 border border-blue-500/30">
+            <div class="border border-white-500/20 mb-5"></div>
             <form method="post" action="" class="grid grid-cols-2 gap-6">
                 
                 
                 <div class="flex flex-col">
-                    <label class="text-blue-500 text-sm mb-2 font-medium">Placa do veículo</label>
-                    <input name="plate" type="text" placeholder="ABC-1234" class="px-4 py-3 bg-slate-900 placeholder-gray-500 border border-slate-700 rounded-lg text-gray-200 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
+                    <label class="text-white text-sm mb-2 font-medium">Placa do veículo</label>
+                    <input name="plate" type="text" placeholder="ABC-1234" class="px-4 py-3 bg-white placeholder-gray-500 border border-sky-5  00 rounded-lg text-gray-200 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
                 </div>
 
                 <div class="flex flex-col">
-                    <label class="text-blue-500 text-sm mb-2 font-medium">Tipo de veículo</label>
-                    <select name="vehicleType" type="string" class="px-4 py-3 bg-slate-900 border border-slate-700 rounded-lg text-gray-200 transition-all duration-200 cursor-pointer">
-                        <option value="car" class="bg-slate-900">Carro</option>
-                        <option value="motorcycle" class="bg-slate-900">Moto</option>
-                        <option value="truck" class="bg-slate-900">Caminhão</option>
+                    <label class="text-white text-sm mb-2 font-medium">Tipo de veículo</label>
+                    <select name="vehicleType" type="string" class="px-4 py-3 bg-white border border-sky-5 00 rounded-lg text-slate-700 transition-all duration-200 cursor-pointer">
+                        <option value="car" class="bg-white">Carro</option>
+                        <option value="motorcycle" class="bg-white">Moto</option>
+                        <option value="truck" class="bg-white">Caminhão</option>
                     </select>
                 </div>
 
                 <div class="flex flex-col">
-                    <label class="text-blue-500 text-sm mb-2 font-medium">Horario de Entrada</label>
-                    <input name="entryTime" type="datetime-local" class="px-4 py-3 bg-slate-900 border border-slate-700 rounded-lg text-gray-200 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
+                    <label class="text-white text-sm mb-2 font-medium">Horario de Entrada</label>
+                    <input name="entryTime" type="datetime-local" class="px-4 py-3 bg-white border border-sky-500 rounded-lg text-slate-700 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
                 </div>
 
                 <div class="flex flex-col">
-                    <label class="text-blue-500 text-sm mb-2 font-medium">Horas Estacionadas</label>
-                    <input name="parkedHours" type="number" placeholder="0" min="0" step="1" class="px-4 py-3 bg-slate-900 placeholder-white border border-slate-700 rounded-lg text-gray-200 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
+                    <label class="text-white text-sm mb-2 font-medium">Horas Estacionadas</label>
+                    <input name="parkedHours" type="number" placeholder="0" min="0" step="1" class="px-4 py-3 bg-white placeholder-white border border-sky-5   00 rounded-lg text-slate-700 transition-all duration-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 focus:outline-none">
                 </div>
 
                 <div class="p4 flex items-center justify-center col-span-full">
-                    <button type="submit" class="bg-blue-600 hover:bg-blue-700 hover:scale-105 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-200 shadow-lg hover:shadow-blue-900/50">Registrar Sessão</button>
+                    <button type="submit" class="bg-sky-400 hover:bg-sky-500 hover:scale-105 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-200 shadow-lg hover:shadow-blue-900/50">Registrar Sessão</button>
                 </div>
             </form>
             <div class="grid grid-cols-2 gap-6"></div>
